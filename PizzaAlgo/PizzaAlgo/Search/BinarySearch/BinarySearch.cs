08fd24b79c42bbbf66223ecf2ad7fa8289df35a6
benni.owa <benni.owa@web.de>
2019-05-23 19:42:51 +0200

    #2 implement binarysearch

---
+++ PizzaAlgo/PizzaAlgo/Search/BinarySearch/BinarySearch.cs
@@ -0,0 +1,22 @@
+﻿namespace PizzaAlgo.Search.BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int rank(int key, int[] a)
+        {
+            int lo = 0;
+            int hi = a.Length - 1;
+            while(lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (key < a[mid])
+                    hi = mid - 1;
+                else if (key > a[mid])
+                    lo = mid + 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
