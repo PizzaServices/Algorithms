8ee74a7cce6185700572cedeef348f53af862523
benni.owa <benni.owa@web.de>
2019-05-23 20:23:02 +0200

    #6 add ShellSort

---
+++ PizzaAlgo/PizzaAlgo/Sorting/ShellSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PizzaAlgo.Sorting
+{
+    public class ShellSort<T> : SortingBase<T> where T : IComparable<T>
+    {
+        public static void sort(T[] a)
+        {
+            int n = a.Length;
+            int h = 1;
+            while(h < n/3)
+            {
+                h = 3 * h + 1;
+            }
+
+            while(h >= 1)
+            {
+                for(int i = h; i < n; i++)
+                {
+                    for(int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+                    {
+                        exch(a, j, j - h);
+                    }
+                }
+                h /= 3;
+            }
+        }
+    }
+}
