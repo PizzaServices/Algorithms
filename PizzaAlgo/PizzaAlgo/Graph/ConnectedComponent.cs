14a9aaac075722211444d899370112681fbd42b5
benni.owa <benni.owa@web.de>
2019-06-10 11:28:44 +0200

    #15 implement ConnectedComponent

---
+++ PizzaAlgo/PizzaAlgo/Graph/ConnectedComponent.cs
@@ -0,0 +1,50 @@
+﻿namespace PizzaAlgo.Graph
+{
+    public class ConnectedComponent
+    {
+        private bool[] marked;
+        private int[] id;
+        private int count;
+
+        public ConnectedComponent(Graph graph)
+        {
+            marked = new bool[graph.Nodes];
+            id = new int[graph.Nodes];
+
+            for(int i = 0; i < graph.Nodes; i++)
+            {
+                if(!marked[i])
+                {
+                    Dfs(graph, i);
+                    count++;
+                }
+            }
+        }
+
+        public bool Connected(int v, int w)
+        {
+            return id[v] == id[w];
+        }
+
+        public int Id(int node)
+        {
+            return id[node];
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        private void Dfs(Graph graph, int node)
+        {
+            marked[node] = true;
+            id[node] = count;
+            foreach(var w in graph.Adjacency(node))
+            {
+                if (!marked[w])
+                    Dfs(graph, w);
+            }
+        }
+    }
+}
