910f3ec93e35e95d640e9bc2df56d474d8834fcf
benni.owa <benni.owa@web.de>
2019-06-10 13:21:25 +0200

    #18 helper classes for digraphs

---
+++ PizzaAlgo/PizzaAlgo/Graph/Directed/DepthFirstOrder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaAlgo.Graph.Directed
+{
+    public class DepthFirstOrder
+    {
+        private bool[] marked;
+
+        private Queue<int> pre;
+        private Queue<int> post;
+        private Stack<int> reversePost;
+
+        public DepthFirstOrder(Digraph digraph)
+        {
+            pre = new Queue<int>();
+            post = new Queue<int>();
+            reversePost = new Stack<int>();
+            marked = new bool[digraph.Vertices];
+
+            for(int v = 0; v < digraph.Vertices; v++)
+            {
+                if (!marked[v])
+                    Dfs(digraph, v);
+            }
+        }
+
+        public IEnumerable<int> Pre()
+        {
+            return pre;
+        }
+
+        public IEnumerable<int> Post()
+        {
+            return post;
+        }
+
+        public IEnumerable<int> ReversePost()
+        {
+            return reversePost;
+        }
+
+        private void Dfs(Digraph digraph, int vertex)
+        {
+            pre.Enqueue(vertex);
+
+            marked[vertex] = true;
+            foreach(var w in digraph.Adjacency(vertex))
+            {
+                if (!marked[w])
+                    Dfs(digraph, w);
+            }
+
+            post.Enqueue(vertex);
+            reversePost.Push(vertex);
+        }
+    }
+}
